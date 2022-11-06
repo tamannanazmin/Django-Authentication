3b733d3c729eb0a7cf5bba9ac71626b31e310e74
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-11-06 11:43:53 +0600

    views to create new user done

--- djangoauthapi1/account/views.py
+++ djangoauthapi1/account/views.py
@@ -7,5 +7,9 @@ from account.serializers import UserRegistrationSerializer
 class UserRegistrationView(APIView):
     def post(self, request, format = None):
         serializer = UserRegistrationSerializer(data = request.data)
-        return Response({'msg' : 'Registration Success'})
+        if serializer.is_valid(raise_exception = True):
+            user = serializer.save()
+            return Response({'msg': 'Registration Successful'}, status = status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
