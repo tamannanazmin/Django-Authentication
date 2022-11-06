14c8d20f22e2b1a8bac771322e070d77a23ee2c2
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-11-06 12:37:12 +0600

    read_only problem solved but not geting passowrd validation

--- djangoauthapi1/account/serializers.py
+++ djangoauthapi1/account/serializers.py
@@ -4,12 +4,12 @@ from account.models import User
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     # We are wirting this because we need to confirm password field in our Registration Request
-    password2 = serializers.CharField(style = {'input_type' : 'password'}, write_only = True)
+    password2 = serializers.CharField(style = {'input_type' : 'password'}, read_only = True)
     class Meta:
         model = User
         fields = ['email', 'name', 'password', 'password2', 'tc']
         extra_kwargs = {
-            'password' : {'write_only' : True}
+            'password' : {'read_only' : True}
         }
 # Validating Password and Confirm Password while Registration
 
