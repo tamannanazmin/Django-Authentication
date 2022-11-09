7b60121449f1f9bc3dec443f76da0e8df463608d
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-11-09 09:57:24 +0600

    send reset password email worked

--- djangoauthapi1/account/urls.py
+++ djangoauthapi1/account/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name = 'login'),
     path('profile/', UserProfileView.as_view(), name = 'profile'),
     path('changepassword/', UserChangePasswordView.as_view(), name = 'changepassword'),
-    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name = 'send-reset-password-email/'),
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name = 'send-reset-password-email'),
 
 ]
