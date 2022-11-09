c0efad8fe5f373bd7fe4e6a5923ca1f7c3daf8f4
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-11-09 11:24:05 +0600

    reset password done in serializer

--- djangoauthapi1/account/urls.py
+++ djangoauthapi1/account/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name = 'profile'),
     path('changepassword/', UserChangePasswordView.as_view(), name = 'changepassword'),
     path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name = 'send-reset-password-email'),
+    path('reset-password/<uid>/<token>/', SendPasswordResetEmailView.as_view(), name = 'reset-password'),
 
 ]
--- djangoauthapi1/account/views.py
+++ djangoauthapi1/account/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from account.serializers import SendPasswordResetEmailSerializer, UserChangePasswordSerializer, UserLoginSerializer, UserRegistrationSerializer, UserProfileSerializer
+from account.serializers import UserPasswordResetSerializer, SendPasswordResetEmailSerializer, UserChangePasswordSerializer, UserLoginSerializer, UserRegistrationSerializer, UserProfileSerializer
 from django.contrib.auth import authenticate
 from account.renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -66,3 +66,10 @@ class SendPasswordResetEmailView(APIView):
         if serializer.is_valid(raise_exception = True):
             return Response({'msg' : 'Password reset link send. Please check your Email'}, status = status.HTTP_200_OK)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+# class UserPasswordResetView(APIView):
+#     renderer_classes = [UserRenderer]
+#     def post(self, request, uid, token, format = None):
+#         serializer = UserPasswordResetSerializer(data= request.data, context = {'uid' : uid, 'token' : token})
+#         serializer.is_valid(raise_exception = True)
+#         return Response({'msg' : 'Password Reset Successfully'}, status = status.HTTP_200_OK)
