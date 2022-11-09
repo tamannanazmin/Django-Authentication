ffed307eafab3f48bace201d8dd422cb08e7bfef
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-11-09 09:31:07 +0600

    one error solved

--- djangoauthapi1/account/serializers.py
+++ djangoauthapi1/account/serializers.py
@@ -68,8 +68,35 @@ class SendPasswordResetEmailSerializer(serializers.Serializer):
         print('Encoded UID: ', uid)
         token = PasswordResetTokenGenerator().make_token(user)
         print('Password Reset Token', token)
-        link = 'http://127.0.0.1:8000/reset/'+uid+'/'+token
+        link = 'http://localhost:3000/reset/'+uid+'/'+token
         print('Password Reset Link', link)
         return attrs
        else:
         raise ValidationErr('You are not a Registered User')
+
+'''class SendPasswordResetEmailSerializer(serializers.Serializer):
+  email = serializers.EmailField(max_length=255)
+  class Meta:
+    fields = ['email']
+
+  def validate(self, attrs):
+    email = attrs.get('email')
+    if User.objects.filter(email=email).exists():
+      user = User.objects.get(email = email)
+      uid = urlsafe_base64_encode(force_bytes(user.id))
+      print('Encoded UID', uid)
+      token = PasswordResetTokenGenerator().make_token(user)
+      print('Password Reset Token', token)
+      link = 'http://localhost:3000/api/user/reset/'+uid+'/'+token
+      print('Password Reset Link', link)
+      # Send EMail
+      body = 'Click Following Link to Reset Your Password '+link
+      data = {
+        'subject':'Reset Your Password',
+        'body':body,
+        'to_email':user.email
+      }
+      # Util.send_email(data)
+      return attrs
+    else:
+      raise serializers.ValidationError('You are not a Registered User')'''
--- djangoauthapi1/account/urls.py
+++ djangoauthapi1/account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from account.views import UserChangePasswordView, UserRegistrationView, UserLoginView, UserProfileView
+from account.views import SendPasswordResetEmailView, UserChangePasswordView, UserRegistrationView, UserLoginView, UserProfileView
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name = 'register'),
     path('login/', UserLoginView.as_view(), name = 'login'),
     path('profile/', UserProfileView.as_view(), name = 'profile'),
     path('changepassword/', UserChangePasswordView.as_view(), name = 'changepassword'),
-    path('send-reset-pasword-email/', UserChangePasswordView.as_view(), name = 'send-reset-pasword-email'),
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name = 'send-reset-password-email/'),
 
 ]
